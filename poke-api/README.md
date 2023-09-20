9f5bc25b11f420ae3eb2551e9cd92216f0fa81ba
jgabrielgavao <josegabriel755@gmail.com>
2023-09-20 18:58:18 -0300

    pokeApi

---
+++ poke-api/README.md
@@ -0,0 +1,55 @@
+
+# PokeApi
+
+Uma página construída com react e utilizando uma api para mostrar diversas informações sobre o pokemon que você quiser!
+
+## Documentação da API
+
+## Stack utilizada
+
+**Front-end:** React;
+## API UTILIZADA
+
+https://pokeapi.co/
+
+#### loadAPI(poke)
+
+Recebe um nome como parâmetro e retorna as informações do pokemon desejado.
+#### Retorna um item
+
+```http
+  GET https://pokeapi.co/api/v2/pokemon/${poke}
+```
+
+| Parâmetro   | Tipo       | Descrição                                   |
+| :---------- | :--------- | :------------------------------------------ |
+| `name`      | `string` | **Obrigatório**. O nome do pokemon |
+
+
+## Instalação
+
+Instale dessa forma
+
+```bash
+  git clone nomedorepo
+  cd pokeApi
+  cd poke-api 
+  npm install
+```
+
+## Execução
+
+Execute à partir de um comando
+
+```bash
+    npm start
+```
+
+### `npm start`
+
+Roda o app em modo de desenvolvimento.\
+Abre [http://localhost:3000](http://localhost:3000) para visualizar em seu navegador.
+
+## Screenshots
+
+![App Screenshot](https://via.placeholder.com/468x300?text=App+Screenshot+Here)
---
+++ poke-api/src/App.js
@@ -0,0 +1,66 @@
+import "./style.css";
+import React, { useEffect, useState } from "react";
+
+function App() {
+  const [pokemon, setPokemon] = useState({});
+  const [valorInput, setValorInput] = useState("");
+
+  function loadApi(poke) {
+    let url = `https://pokeapi.co/api/v2/pokemon/${poke}`;
+    fetch(url)
+      .then((response) => response.json())
+      .then((json) => {
+        console.log(json);
+        setPokemon(json);
+      })
+      .catch((err) => console.log(err));
+  }
+
+  const handleInputChange = (event) => {
+    setValorInput(event.target.value);
+  };
+
+  const buscarPokemon = () => {
+    loadApi(valorInput);
+  };
+
+  useEffect(() => {
+    loadApi("charizard");
+  }, []);
+
+  return (
+    <div className="container">
+      <header>
+        <strong>POKEMON API</strong>
+      </header>
+      <br />
+      <div className="painel"></div>
+      <div className="conteudo">
+      <div className="busca">
+        <input
+        className="inputPoke"
+          id="nomePokemon"
+          placeholder="charizard"
+          type="text"
+          value={valorInput}
+          onChange={handleInputChange}
+        />
+        <button className="buttonPoke" type="button" onClick={buscarPokemon}>
+          BUSCAR
+        </button>
+      </div>
+      <div className="pokeForm">
+      <img className="imagemPoke" src={pokemon.sprites?.front_default} alt={pokemon.name} /><br/>
+      <div className="stats"><strong>NOME:</strong> {pokemon.name}</div>
+      <div className="stats"><strong>N°:</strong> {pokemon.id}</div>
+      <div className="stats"><strong>PESO:</strong> {pokemon.weight / 10}kg</div>
+      <div className="stats"><strong>ALTURA:</strong>: {pokemon.height / 10}m</div>
+      <div className="stats"><strong>HP:</strong> {pokemon.stats ? pokemon.stats[0]?.base_stat : null}</div>
+      <div className="stats"><strong>ATTACK:</strong> {pokemon.stats ? pokemon.stats[1]?.base_stat : null}</div>
+      </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
